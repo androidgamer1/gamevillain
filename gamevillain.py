c2c713f35ac10639e73ed1dab13aae4df15608d9
swapnilyt123 <48355404+swapnilyt123@users.noreply.github.com>
2019-03-19 21:16:16 +0530

    Update gamevillain.py

--- gamevillain.py
+++ gamevillain.py
@@ -24,7 +24,7 @@ client.remove_command('help')
 
 async def status_task():
     while True:
-        await client.change_presence(game=discord.Game(name='for A!help'))
+        await client.change_presence(game=discord.Game(name='for G!help'))
         await asyncio.sleep(5)
         await client.change_presence(game=discord.Game(name='with '+str(len(set(client.get_all_members())))+' users'))
         await asyncio.sleep(5)
